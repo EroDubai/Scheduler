ed084e4fd765e8459037c2d7947c18481bf735f9
unknown <you@example.com>
2018-04-30 14:36:11 -0500

    first commit

---
+++ src/com/ownk/schedule/Abc.java
@@ -0,0 +1,25 @@
+/*
+ * MIT License 
+ * 
+ * Copyright (c) 2018 Jo&Da,INC
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ */
+
+package com.ownk.schedule;
+
+/**
+ *
+ * <h1>title
+ *
+ * Description
+ *
+ * @author david.piracoca@ownk.com (EroDubai) 
+ * @version 1.0
+ * 
+ */
+public class Abc {
+
+}
